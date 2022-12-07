04b9fe66a5262a1dfdebb87b1ac34e7d291537bb
olgaqa <olgmelnyk@gmail.com>
2022-12-07 16:28:57 -0500

    Initial commit

---
+++ H4X0R News/H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Olga Melnyk on 10.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
